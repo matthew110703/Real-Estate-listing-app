208b59e586a292ac4a9ee8db8e9cb4a022311e31
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-03-13 14:47:38 +0530

    Designed Login Page UI

---
+++ src/components/forms/LoginForm.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+
+// UI
+import { Button, Input, IconButton } from "../ui";
+
+// Icons
+import {
+  MdOutlineMail,
+  MdVisibility,
+  MdVisibilityOff,
+  MdLogin,
+} from "react-icons/md";
+import { TbLockPassword } from "react-icons/tb";
+
+const LoginForm = ({ onSubmit, loading = false }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Form
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onSubmit(form).finally(() => {
+      setForm({
+        email: "",
+        password: "",
+      });
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-y-8">
+      <Input
+        type="email"
+        name="email"
+        label="Email"
+        placeholder="example@gmail.com"
+        startAdornment={<MdOutlineMail size={24} />}
+        required
+        value={form.email}
+        onChange={handleChange}
+      />
+      <Input
+        type={showPassword ? "text" : "password"}
+        name="password"
+        label="Create Password"
+        placeholder="••••••••"
+        startAdornment={<TbLockPassword size={24} />}
+        endAdornment={
+          <IconButton
+            icon={showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+            onClick={() => setShowPassword(!showPassword)}
+          />
+        }
+        minLength={8}
+        maxLength={20}
+        required
+        value={form.password}
+        onChange={handleChange}
+      />
+      <Button
+        type={"submit"}
+        text={loading ? "Loading..." : "Sign Up"}
+        icon={!loading && <MdLogin size={24} />}
+        disabled={loading}
+      />
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router-dom";
+
+// UI
+import { Button, Input, Branding } from "../components/ui";
+
+// Form
+import { LoginForm } from "../components/forms";
+
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = async (form) => {
+    console.log(form);
+  };
+
+  return (
+    <section className="flex h-[90vh] flex-col items-center justify-center gap-y-8">
+      <Branding />
+      <LoginForm onSubmit={handleSubmit} />
+      <p>
+        Already have an account?{" "}
+        <Link
+          to="/signup"
+          className="font-semibold underline-offset-2 hover:underline"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </section>
+  );
 };
 
 export default Login;
