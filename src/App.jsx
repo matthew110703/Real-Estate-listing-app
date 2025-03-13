809074fbf2c3d32646606c69db31c4f8a2c3e794
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-03-13 13:23:42 +0530

    Initialized Pages and React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Pages
+import { SignUp, Login, Dashboard, PropertyDetail, NotFound } from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/property/:id" element={<PropertyDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+// Pages
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+import PropertyDetail from "./PropertyDetail";
+import NotFound from "./NotFound";
+
+export { SignUp, Login, Dashboard, PropertyDetail, NotFound };
